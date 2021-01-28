dbd0f04f3ce4376f707d6f707c4a2bfef2d02fd2
BrianQuintero <63617101+BrianQuintero@users.noreply.github.com>
2021-01-28 00:07:53 -0800

    lab 4 & lab 5

---
+++ lab4/js/lab4.js
@@ -0,0 +1,12 @@
+//Turn on your computer
+//Log in to your account on your computer
+//Wait for all the fun startup programs to start up
+//Right-click on any empty spot in your desktop
+//Navigate your mouse to the "Personalize" button and left-click
+//Scroll down to the section that says "Background"
+//Find the image you are looking for
+  //If the image you are looking for isn't there, click on the "Browse" button
+    //Look for the image that you are looking for using the file browser
+      //NOTE: I'm assuming you know how to use the file browser. If you don't, then God help you
+//Double click on the image you want
+//Enjoy you new background
